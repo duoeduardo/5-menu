4d15e13752b20aa7c5b1de77105f5a980f54d0df
Eduardo <duoeduardo@gmail.com>
2020-11-12 19:03:59 -0300

    Se agrega filtro por categoria

--- src/App.js
+++ src/App.js
@@ -3,9 +3,22 @@ import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
 
+const allCategories =['all', ...new Set(items.map((item) => item.category))] ;
+console.log(allCategories)
+
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItem = items.filter((item) => item.category === category);
+    setMenuItems(newItem);
+  };
 
   return (
     <main>
@@ -14,8 +27,8 @@ function App() {
           <h2>Our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories />
-        <Menu items={menuItems}/>
+        <Categories filterItems={filterItems} categories={categories}/>
+        <Menu items={menuItems} />
       </section>
     </main>
   );
